bd949e09753bbd4edbe8018db7a6a417334609d9
Moheed789 <moheedsiddiqui74@gmail.com>
2024-06-10 20:53:12 +0500

    Add post confirmation and iam role permissions and remove custom attribute

--- src/trigger/postConfirmation.js
+++ src/trigger/postConfirmation.js
@@ -1,12 +1,13 @@
 import AWS from "aws-sdk";
 import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import hhtpErrorHandler from "@middy/http-error-handler";
 import { v4 as uuidv4 } from "uuid";
 import httpErrorHandler from "@middy/http-error-handler";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
-const tableName = process.env.event_planner_table;
-const groupName = process.env.event_group_name;
+const eventTableName = process.env.event_planner_table;
+const eventGroupName = process.env.event_group_name;
+const userTableName = process.env.user_ticket_purchaser_table;
+const userGroupName = process.env.user_ticket_purchaser_group_name;
+const profile = "eventplanner";
 
 const handler = async (event) => {
   console.log("event", JSON.stringify(event));
@@ -18,27 +19,44 @@ const handler = async (event) => {
       fatherName: event.request.userAttributes.family_name,
       email: event.request.userAttributes.email
     };
-    await dynamodb.put({
-      TableName: tableName,
-      Item: userCreate
-    }).promise();
-    const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
-    const params = {
-      GroupName: groupName,
-      Username: event.userName,
-      UserPoolId: event.userPoolId
-    };
-    await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
-    console.log('User added to group successfully.');
-    return event
+    if (profile === "eventplanner") {
+      await dynamodb.put({
+        TableName: eventTableName,
+        Item: userCreate
+      }).promise();
+      const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+      const params = {
+        GroupName: eventGroupName,
+        Username: event.userName,
+        UserPoolId: event.userPoolId
+      };
+      await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
+      console.log('User added to group successfully.');
+      return event
+    } else if (profile === "userTicketPurchaser") {
+      await dynamodb.put({
+        TableName: userTableName,
+        Item: userCreate
+      }).promise();
+      const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+      const params = {
+        GroupName: userGroupName,
+        Username: event.userName,
+        UserPoolId: event.userPoolId
+      };
+      await cognitoIdentityServiceProvider.adminAddUserToGroup(params).promise();
+      console.log('User added to group successfully.');
+      return event
+    } else {
+      throw err;
+    }
   } catch (err) {
     console.error('Error', err)
     throw err;
   }
 };
 
-export default {
-  handler: middy(handler)
-  .use(httpJsonBodyParser())
-  .use(httpErrorHandler())
-}
+export const postConfirmationTrigger = middy(handler)
+  .use(httpErrorHandler());
+
+export default postConfirmationTrigger;
